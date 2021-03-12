6cfc6fae3fb97ef8a9b9cd0b24342a5a335dc8ce
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-03-12 12:16:49 -0800

    Display the year in the footer by getting the year via JS using the Date Object

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 function Footer() {
   return (
     <div className="footer">
-      <p>Kaniet Oskonbaev copyright 2021</p>
+      <p>Kaniet Oskonbaev copyright {(new Date().getFullYear())}</p>
           <div className="Footer-Subtitle">
            San Franciscos Privately Owned Public Spaces
            </div>
