aa1b5cb34b0727fbfca4b34ed9e7bb2ce6d930d3
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-03-12 23:33:53 -0800

    nav-link-active

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import './App.css';
 import Title from './Title';
 import POPOSList from './POPOSList';
 import Footer from './Footer'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import About from './About'
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Title />
-      <POPOSList />
+      <Route exact path="/" component={POPOSList}/>
+      <Route path="/about" component={About} />
       < Footer/>
     </div>
+    </Router>
   );
 }
 
--- src/Title.js
+++ src/Title.js
@@ -1,14 +1,29 @@
 // src/Title.js
 import React from 'react'
 import './Title.css';
+import { NavLink } from 'react-router-dom'
 
 function Title() {
   return (
     <div className="Title">
-      <h1>SFPOPOS</h1>
-          <div className="Title-Subtitle">
-           San Franciscos Privately Owned Public Spaces
-           </div>
+      <header>
+        <h1>SFPOPOS</h1>
+        <div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
+
+        <div>
+          <NavLink
+            className="nav-link"
+            activeClassName="nav-link-active"
+            exact
+            to="/">List</NavLink>
+
+            <NavLink
+              className="nav-link"
+              activeClassName="nav-link-active"
+              to="/about">About</NavLink>
+        </div>
+
+      </header>
     </div>
   )
 }
