9cc753cdcd999943a3058300237b2af7a2dbcab9
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-03-13 18:42:27 -0800

    styling spaces

--- src/components/poposspace/POPOSSpace.js
+++ src/components/poposspace/POPOSSpace.js
@@ -1,18 +1,19 @@
 // src/Project.js
 
 import React from 'react'
-import '../poposspace/POPOSSpace';
 import { Link } from 'react-router-dom'
+import './POPOSSpace.css'
 
 function POPOSSpace(props) {
  const { name, image, address, hours, id } = props
   return (
-    <div>
-      <Link to={`/details/${id}`}>
+    <div className="POPOSSpace">
+      <Link className="POPOSSpace-title" to={`/details/${id}`}>
         <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+        <h1 >{name}</h1>
       </Link>
-      <h1>{name}</h1>
-      <div>{address}</div>
+      
+      <div className="POPOSSpace-info">{address}</div>
       <div className = 'hours'>{hours}</div>
     </div>
   )
