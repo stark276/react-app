9af13b6ed33efa6a4cfbcf624ebd6896b4c903de
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-03-14 23:10:48 -0700

    Searching Spaces

--- src/components/poposdetails/POPOSDetails.js
+++ src/components/poposdetails/POPOSDetails.js
@@ -1,26 +1,24 @@
 // src/POPOSDetails.js
 
 import React from 'react'
+import './POPOSDetails.css'
 
 import data from '../../sfpopos-data.json'
 function POPOSDetails(props) {
   const { id } = props.match.params // Location index
   const { images, title, desc, hours, features, geo } = data[id]
-
   return (
-    <div>
-      <div>
+    <div className="POPOSDetails">
+      <div className="POPOSDetails-image">
         <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} />
       </div>
-
-      <div>
-        <h1>{ title }</h1>
-        <p>{ desc }</p>
-        <p>{ hours }</p>
-        <p>{ features }</p>
-        <p>{ geo.lat } { geo.lon }</p>
+      <div className="POPOSDetails-info">
+        <h1 className="POPOSDetails-title">{ title }</h1>
+        <p className="POPOSDetails-desc">{ desc }</p>
+        <p className="POPOSDetails-hours">{ hours }</p>
+        <p className="POPOSDetails-features">{ features }</p>
+        <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
       </div>
-
     </div>
   )
 }
--- src/components/poposlist/POPOSList.js
+++ src/components/poposlist/POPOSList.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react'
 import POPOSSpace from '../poposspace/POPOSSpace';
 import './POPOSList.css';
 import data from '../../sfpopos-data.json'
 
 
 function POPOSList() {
-
+  const [ query, setQuery ] = useState('')
   const spaces = data.map(({ title, address, images, hours }, i) => {
   return (
     <POPOSSpace
@@ -21,7 +21,15 @@ function POPOSList() {
 
   return (
     <div className="POPOSList">
-      { spaces }
+            <form>
+                <input
+                    value={query}
+                    placeholder="search"
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            {spaces}
     </div>
   )
 }
